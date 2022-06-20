9aa2026dd3902a21cf15f175ac1ac8661e3f6ad2
unknown <muhammadfathullah171217@gmail.com>
2022-06-20 14:05:55 +0700

    first commit

---
+++ src/main/java/id/co/mii/ta/ticketingserverside/TicketingServerSideApplication.java
@@ -0,0 +1,13 @@
+package id.co.mii.ta.ticketingserverside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingServerSideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketingServerSideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/co/mii/ta/ticketingserverside/TicketingServerSideApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.mii.ta.ticketingserverside;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingServerSideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
